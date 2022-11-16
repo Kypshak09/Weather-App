09ea601f9e9368c3362cae71e141eff3032bb7de
Kypshak09 <amirzhunusov09@gmail.com>
2022-11-16 15:48:15 +0600

    Initial Commit

---
+++ Weather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Amir Zhunussov on 16.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
